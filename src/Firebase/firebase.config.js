588b1b827fbec6bd9fd615096b29c65d33cc1644
Tareq Mahmud <40931112+Mahmudtareq@users.noreply.github.com>
2022-03-24 01:12:41 +0600

    Update firebase.config.js

--- src/Firebase/firebase.config.js
+++ src/Firebase/firebase.config.js
@@ -1,10 +1,24 @@
 const firebaseConfig = {
   apiKey: "AIzaSyBRncg8LwtcKbPF2eSNLRD_fyY2pI6_psY",
-  authDomain: "tatiartant.com",
+  authDomain: "tat-ecommece.firebaseapp.com",
   projectId: "tat-ecommece",
   storageBucket: "tat-ecommece.appspot.com",
   messagingSenderId: "825560372378",
   appId: "1:825560372378:web:b8765bffe51b9925347b23"
 };
 export default firebaseConfig;
+
+
+
+
+
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBRncg8LwtcKbPF2eSNLRD_fyY2pI6_psY",
+//   authDomain: "tatiartant.com",
+//   projectId: "tat-ecommece",
+//   storageBucket: "tat-ecommece.appspot.com",
+//   messagingSenderId: "825560372378",
+//   appId: "1:825560372378:web:b8765bffe51b9925347b23"
+// };
+// export default firebaseConfig;
   
